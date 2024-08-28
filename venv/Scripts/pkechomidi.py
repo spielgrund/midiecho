63f16fed06b4265a6cd1ed759879b2b819a2abc0
spielgrund <138134622+spielgrund@users.noreply.github.com>
2024-08-28 17:41:58 +0200

    update

---
+++ venv/Scripts/pkechomidi.py
@@ -0,0 +1,20 @@
+#!C:\Users\Student\Desktop\midi\.venv\Scripts\python.exe
+
+import os
+import sys
+sys.path.append(os.path.join(os.getcwd(), 'tests'))
+import rtmidi
+
+
+def callback(collector, msg):
+    print("%s: %s" % (collector.portName, msg))
+
+
+collectors = rtmidi.CollectorBin()
+collectors.start()
+print('HIT ENTER TO EXIT')
+try:
+    sys.stdin.read(1)
+except KeyboardInterrupt:
+    pass
+collectors.stop()
